ce7950cbb4b4ff62568a2e98148e05eed1b7da0c
mgsonny <smg80zzang@daum.net>
2024-02-21 00:58:14 +0900

    second-commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const API_KEY = `43e023c4728c4c2191bb719a89398da5`
 let news = [];
 
 const getLastestNews = async () => {
-    const url = new URL (`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL (`https://mgson-news-times.netlify.app/top-headlines?${keyword}&country=kr&pageSize=${PAGE_SIZE}`);
     // console.log("uuu", url)
     const response = await fetch (url)
     const data = await response.json()
@@ -12,3 +12,5 @@ const getLastestNews = async () => {
 }
 
 getLastestNews()
+
+https://mgson-news-times.netlify.app/
