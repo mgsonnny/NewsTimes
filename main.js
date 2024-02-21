0ec78690ae5559e0b5dfd1e99430de49501a1f54
mgsonny <smg80zzang@daum.net>
2024-02-21 22:59:19 +0900

    forth-commit

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 
 const API_KEY = `43e023c4728c4c2191bb719a89398da5`
-let news = [];
+let newsList = [];
 let keyword = "아이유";
 let PAGE_SIZE = 20;
 
 const getLastestNews = async () => {
     const url = new URL (`https://mgson-news-times.netlify.app/top-headlines?${keyword}&country=kr&pageSize=${PAGE_SIZE}`);
+    // const url = new URL (`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
     // console.log("uuu", url)
     const response = await fetch (url)
     const data = await response.json()
-    news = data.articles;
-    console.log("ddd", data.articles)
+    newsList = data.articles;
+    render();
+    console.log("ddd", newsList);
 }
 
+const render = ()=>{
+    const newsHTML = newsList.map(news=>`<div class="row news">
+    <div class="col-lg-4">
+        <img class="news-img-size" src=${news.urlToImage}/>
+    </div>
+    <div class="col-lg-8">
+        <h2>${news.title}</h2>
+        <p>
+            ${news.description}
+        </p>
+        <div>
+            ${news.source.name} * ${news.publishedAt}
+        </div>
+    </div>
+</div>`).join('')
+
+
+    document.getElementById("news-board").innerHTML=newsHTML
+}
+
+
+
 getLastestNews()
+
