5bd76a1157e265d1c4b0724428f963a04a2c2ccc
mgsonny <smg80zzang@daum.net>
2024-02-25 02:22:35 +0900

    eight-commit

--- main.js
+++ main.js
@@ -6,9 +6,16 @@ let PAGE_SIZE = 20;
 const menus = document.querySelectorAll(".menus button, .side-menu-list button"); // ".side-menu-list button", 
 menus.forEach(menu=>menu.addEventListener("click",(event)=>getNewsByCategory(event)))
 let url = new URL (`https://mgson-news-times.netlify.app/top-headlines?&country=kr&pageSize=${PAGE_SIZE}`);
+let totalResults = 0
+let page = 1
+const pageSize=10
+const groupSize=5
+
 
 const getNews = async () =>{
     try{
+      url.searchParams.set("page",page) // =>&page = page
+      url.searchParams.set("pageSize", pageSize);
       const response = await fetch(url)
       const data = await response.json();
       if (response.status === 200){
@@ -16,7 +23,9 @@ const getNews = async () =>{
           throw new Error("No result for this search")
         }
         newsList = data.articles;
+        totalResults = data.totalResults
         render();
+        paginationRender();
       } else {
         throw new Error(data.message);
       }
@@ -96,6 +105,50 @@ const errorRender = (errorMessage) => {
   ${errorMessage}
 </div>`;
   document.getElementById("news-board").innerHTML = errorHTML
+}
+const paginationRender=()=>{
+  //totalResult
+  //page
+  //pageSize
+  //totalpage
+  const totalPages = Math.ceil(totalResults/pageSize)
+  //pageGroup
+  const pageGroup = Math.ceil(page/groupSize)
+  //groupSize
+  //lastPage
+  const lastPage = pageGroup * groupSize
+  //마지막 페이지그룹이 그룹사이즈보다 작다? lastpage = totalpage
+  if (lastPage > totalPages){
+    lastPage = totalPages;
+  }
+  //firstPage'
+  const firstPage = lastPage - (groupSize - 1)<=0? 1 : lastPage - (groupSize - 1);
+
+  let paginationHTML = ``
+
+  for (let i = firstPage;i<=lastPage;i++){
+    paginationHTML += `<li class="page-item ${i===page ? "active":''}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`
+  }
+
+  document.querySelector(".pagination").innerHTML = paginationHTML
+
+
+  // <nav aria-label="Page navigation example">
+  //           <ul class="pagination">
+  //             <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+  //             <li class="page-item"><a class="page-link" href="#">1</a></li>
+  //             <li class="page-item"><a class="page-link" href="#">2</a></li>
+  //             <li class="page-item"><a class="page-link" href="#">3</a></li>
+  //             <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  //           </ul>
+  //         </nav>
+
+}
+const moveToPage = (pageNum) => {
+  console.log("page", pageNum)
+  page = pageNum
+  getNews()
+
 }
 
 getLatestNews()
